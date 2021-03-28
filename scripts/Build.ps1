3f0f0a66daa87b47849ae34bfb2f6bf5f367e0dd
Jon Thysell <thysell@gmail.com>
2021-03-28 12:54:50 -0700

    Added error checking to build script

--- scripts/Build.ps1
+++ scripts/Build.ps1
@@ -21,9 +21,18 @@ if (Test-Path "$OutputRoot\$TargetOutputDirectory") {
 
 Write-Host "Build release..."
 
-dotnet msbuild $BuildArgs.Split() -restore -p:Configuration=Release -p:PublishDir="$RepoRoot\$OutputRoot\$TargetOutputDirectory" "$SolutionPath"
-Copy-Item "README.md" -Destination "$OutputRoot\$TargetOutputDirectory\ReadMe.txt"
-Copy-Item "LICENSE.md" -Destination "$OutputRoot\$TargetOutputDirectory\License.txt"
-Copy-Item "CHANGELOG.md" -Destination "$OutputRoot\$TargetOutputDirectory\ChangeLog.txt"
+try
+{
+    dotnet msbuild $BuildArgs.Split() -restore -p:Configuration=Release -p:PublishDir="$RepoRoot\$OutputRoot\$TargetOutputDirectory" "$SolutionPath"
+    if (!$?) {
+    	throw 'Build failed!'
+    }
 
-Set-Location -Path "$StartingLocation"
+    Copy-Item "README.md" -Destination "$OutputRoot\$TargetOutputDirectory\ReadMe.txt"
+    Copy-Item "LICENSE.md" -Destination "$OutputRoot\$TargetOutputDirectory\License.txt"
+    Copy-Item "CHANGELOG.md" -Destination "$OutputRoot\$TargetOutputDirectory\ChangeLog.txt"
+}
+finally
+{
+    Set-Location -Path "$StartingLocation"
+}
